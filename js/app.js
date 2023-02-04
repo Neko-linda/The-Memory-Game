38e4b4581c3ff477412c7f362efe95b9de47e726
User Name <emelinda.gutierrez04@gmail.com>
2023-02-03 19:23:58 -0500

    Added a flip card function

--- js/app.js
+++ js/app.js
@@ -9,37 +9,74 @@
 
 // document.addEventListener('DOMContentLoaded', () => {
 
-    const gameElements = {
+    // const gameElements = {
 
-        gameStart: document.querySelector('button'),
-        gameboard : document.querySelector('gameBoard'),
-        moves: document.querySelector('moveCounter'),
-        timer: document.querySelector('timer')
-    }
+    //     gameStart: document.querySelector('button'),
+    //     gameboard : document.querySelector('gameBoard'),
+    //     moves: document.querySelector('moveCounter'),
+    //     timer: document.querySelector('timer')
+    // }
 
+
+    const timer = document.querySelector('.timer')
     const card = document.querySelectorAll('.cardTile')
+    const frontOfCard = document.querySelectorAll('.front')
+    const backOfCard = document.querySelectorAll('.back')
 
-    //adding a function for card flipping
-        
-    function flipCard() {
-        this.classList.toggle('flip');
-    }
-
-    cards.forEach(card => card.addEventListener('click', flipCard));
+    //adding a function for card flipping and also shuffling the images after restarting the game.
 
 
     shuffleImage()
-    function shuffleImage () {
-        card.forEach(card => {
+    flipCard()
+    function shuffleImage() {
+        card.forEach(c => {
 
             const num = [...Array(card.length).keys()]
             const random = Math.floor(Math.random()*card.length)
             
-            card.style.order = num[random]
+            c.style.order = num[random]
 
         })
     }
 
+    function flipCard() {
+
+        for(let i = 0; i<card.length; i++) {
+            
+            card[i].addEventListener('click',()=>{
+                frontOfCard[i].classList.add('flip')
+                const flippedCard = document.querySelectorAll('.flip')
+
+                if(flippedCard.length == 2){
+                    checkForMatch(flippedCard[0], flippedCard[1]) //here is where we add a callback for the finding a match function
+                }
+            })
+        }
+    }
+
+
+    //Adding a function for the "Matching" call in the flip card function
+
+    function checkForMatch(firstCard , secondCard) {
+    //used settimeout to show the player that they chose the wrong pairs https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
+    if(firstCard.dataset.index == secondCard.dataset.index) {
+        firstCard.classlist.remove('flip')
+        secondCard.classList.remove('flip')
+
+        firstCard.classList.add('checkForMatch')
+        secondCard.classList.add('checkForMatch')
+    }else{
+            setTimeout(() => {
+            firstCard.classlist.remove('flip')
+            secondCard.classList.remove('flip')
+        }, 1000);
+    }
+}
+
+    // ---------
+
+
+    //THIS IS OLD PSUEDO CODE: I decided to put the cards in the HTML
 //Card objects using arrays and relative pathing
 
 // const cardOptions = [
@@ -54,7 +91,7 @@
 //     {name: 'sunflower', Img: 'IconImages/KawaiiSunflower.png'},
 // ];
 
-//I decided to put the cards in the HTML
+
 
 //Initial stats when game starts
 
